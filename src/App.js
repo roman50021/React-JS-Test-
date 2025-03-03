de012721790c19e8e550b182edc8590d37fcc370
Roman <fedkoroma2@gmail.com>
2025-03-03 15:57:09 +0200

    Test 7

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
-import Image from "./components/Image";
-import logo from "./img/logo.png"
+import Users from "./components/Users";
 
 class App extends React.Component{
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            helpText: "Help text",
-            userData: ""
-        }
-        this.inputClick= this.inputClick.bind(this)
-    }
-
-    helpText = "Help text!"
-
     render() {
-        return (<div className="name">
-            <Header title="Head of App website"/>
-            <h1>{this.state.helpText}</h1>
-            <h2>{this.state.userData}</h2>
-            <input placeholder={this.state.helpText}
-                   onChange={event => this.setState({userData: event.target.value})}
-                   onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-            <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
-            <Image image={logo}/>
+        return (<div>
+            <Header title="List of users: "/>
+            <main>
+                <Users/>
+            </main>
+            <aside></aside>
         </div>)
     }
 
-    inputClick(){
-        this.setState({helpText: "Changed" })
-        this.helpText = "Changed"
-        console.log("Clicked")
-    }
-    mouseOver(){ console.log("Mouse Over")}
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Button from "./Button";
 
 class Header extends React.Component{
     render() {
         return(
             <header className={"header"}>
                 {this.props.title}
-                <Button text="Button"/>
 
             </header>
         )
